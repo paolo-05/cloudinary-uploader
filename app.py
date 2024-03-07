de70bd860cbead454e5e4909d374c7f38692714f
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2024-03-07 09:23:51 +0100

    vercel doesn't like flask..

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import request, jsonify
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 import cloudinary
-from . import app
+import cloudinary.api
+import cloudinary.uploader
+
+# Load environment variables from .env file
+load_dotenv()
+
+cloudinary.config(
+  cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+  api_key = os.getenv("CLOUDINARY_API_KEY"),
+  api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+  secure=True
+)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif', 'mp4'}
+
 
 @app.route('/')
 def hello():
@@ -10,15 +28,15 @@ def hello():
 def upload_file():
     # Check if Authorization header is present
     auth_header = request.headers.get('Authorization')
-    if not auth_header or not auth_header.startswith('Bearer '):
+    if not auth_header or not auth_header.startswith('Bearer'):
         return jsonify({"error": "Unauthorized"}), 401
     
     # Extract token from Authorization header
-    token = auth_header.split(' ')[1]
+    token = auth_header.split(': ')[1]
 
     # Here, you can validate the token, for example, using JWT or any other method
     ## NO ACTUAL VALIDATION.
-    if token is '':
+    if token == '':
         return jsonify({"error": "Unauthorized"}), 401
 
     file_to_upload = request.files['file']
@@ -29,3 +47,5 @@ def upload_file():
     else:
         return jsonify({"error": "No file provided"}), 400
 
+if(__name__ == '__main__'):
+    app.run()
